5d0f2d281712526ca787cf298a37e72ca6561afd
sahas052 <sahas052@gmail.com>
2021-04-25 14:28:45 +0530

    Bootstrapping netflix-clone

---
+++ src/Counter.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Counter() {
+  const [count, setCount] = React.useState(0);
+  <div>
+    <p> Current Counter</p>
+    <button onClick="">Increment</button>
+  </div>;
+}
+
+export default Counter;
